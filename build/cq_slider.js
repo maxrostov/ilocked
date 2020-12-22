b194d28193ed291b8cb2262aa7133ee500551264
max <max@nitro.ru>
2020-12-22 07:27:40 +0300

    исправил слайдеры
    Took 5 hours 45 minutes

--- build/cq_slider.js
+++ build/cq_slider.js
@@ -20,21 +20,23 @@ var cq_swiper_quest = new Swiper('.cq_swiper_quest', {
     // },
 });
 
-var cq_swiper_included = new Swiper('.cq_swiper_included', {
+var cq_swiper_included = new Swiper('.cq_included_swiper', {
     // Optional parameters
     // direction: 'vertical',
     loop: true,
+    height:850,
     grabCursor: true,
+    // autoHeight:true,
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
     },
-
-
-
-    breakpoints: {
-        640: {
-            slidesPerView: 1,
+    // centeredSlides: true,
+    // spaceBetween: 10,
+    // slidesPerView: 3,
+   breakpoints: {
+        0: {
+            slidesPerView: 1.2,
             spaceBetween: 10,
         },
         800: {
@@ -46,20 +48,62 @@ var cq_swiper_included = new Swiper('.cq_swiper_included', {
             spaceBetween: 20,
         }}
 });
-//
-// $('.owl-carousel').owlCarousel({
-//     loop:true,
-//     margin:10,
-//     nav:true,
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         600:{
-//             items:2
-//         },
-//         1000:{
-//             items:3
-//         }
-//     }
-// })
+
+
+var cq_swiper_feedback = new Swiper('.cq_swiper_feedback', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+    // height:700,
+    grabCursor: true,
+    // autoHeight:true,
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    // centeredSlides: true,
+    // spaceBetween: 10,
+    // slidesPerView: 3,
+    breakpoints: {
+        0: {
+            slidesPerView: 1.2,
+            spaceBetween: 10,
+        },
+        800: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        }}
+});
+
+
+if (window.innerWidth < 960) {
+// слайдер только для мобильного,
+// для пакетов (для пакетов, на десктопе это таблица)
+var swiper_table_mob = new Swiper('.cq_table_mob', {
+    loop: true,
+    grabCursor: true,
+    // this shows a bit of the previous/next slides
+    slidesPerView: 1.2,
+    centeredSlides: true,
+    spaceBetween: 10,
+    });
+
+    var swiper_table_mob = new Swiper('.cq_included_swiper_mobile', {
+        loop: true,
+        grabCursor: true,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        // this shows a bit of the previous/next slides
+        slidesPerView: 1,
+        centeredSlides: true,
+        spaceBetween: 10,
+    });
+
+}
+
+
